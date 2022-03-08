aafe21340e307388cde10624a80d6d2f8dd293ba
Nazzala Ihsan <nazzalaihsan123@gmail.com>
2022-03-08 08:32:52 +0700

    test

---
+++ app/src/main/java/com/Ihsan/ujikom/buttonThreeActivity.kt
@@ -0,0 +1,11 @@
+package com.Ihsan.ujikom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class buttonThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button_three)
+    }
+}
