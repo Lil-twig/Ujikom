5ff2e614767bfafba86f3ba08f005f8903e30918
mughits06 <mughitszufar@gmail.com>
2022-03-15 15:05:34 +0700

    test

--- app/src/main/java/com/Ihsan/ujikom/activity/SignInActivity.kt
+++ app/src/main/java/com/Ihsan/ujikom/activity/SignInActivity.kt
@@ -2,7 +2,9 @@ package com.Ihsan.ujikom.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.Ihsan.ujikom.R
+import com.Ihsan.ujikom.User
 import com.Ihsan.ujikom.databinding.ActivitySignInBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -24,6 +26,19 @@ class SignInActivity : AppCompatActivity() {
             val namaAnak = binding.signAnak.text.toString()
 
             database = FirebaseDatabase.getInstance().getReference("Users")
+            val User = User(namaOrangtua,password,namaAnak)
+            database.child(namaAnak).setValue(User).addOnSuccessListener {
+
+                binding.signNamaOrangTua.text.clear()
+                binding.signPw.text.clear()
+                binding.signAnak.text.clear()
+
+                Toast.makeText(this,"Succesfully Saved", Toast.LENGTH_SHORT).show()
+
+            }.addOnFailureListener {
+                Toast.makeText(this,"Failed", Toast.LENGTH_SHORT).show()
+
+            }
 
 
 
