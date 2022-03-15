d88536f96c7f31e32fb9f5be4cb6deef2783cd15
Nazzala Ihsan <nazzalaihsan123@gmail.com>
2022-03-15 10:08:46 +0700

    test

--- app/src/main/java/com/Ihsan/ujikom/activity/SignInActivity.kt
+++ app/src/main/java/com/Ihsan/ujikom/activity/SignInActivity.kt
@@ -3,13 +3,31 @@ package com.Ihsan.ujikom.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.Ihsan.ujikom.R
+import com.Ihsan.ujikom.databinding.ActivitySignInBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class SignInActivity : AppCompatActivity() {
 
-
+    private lateinit var binding : ActivitySignInBinding
+    private lateinit var database : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(binding.root)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+
+        binding.btnSignUp.setOnClickListener {
+
+            val namaOrangtua = binding.signNamaOrangTua.text.toString()
+            val password = binding.signPw.text.toString()
+            val namaAnak = binding.signAnak.text.toString()
+
+            database = FirebaseDatabase.getInstance().getReference("Users")
+
+
+
+        }
+
     }
 }
