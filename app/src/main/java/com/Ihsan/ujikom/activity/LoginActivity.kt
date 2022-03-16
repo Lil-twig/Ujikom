c2a1c800d3c4ece170684a793c70d854b9973e2b
mughits06 <mughitszufar@gmail.com>
2022-03-16 09:13:49 +0700

    test

--- app/src/main/java/com/Ihsan/ujikom/activity/LoginActivity.kt
+++ app/src/main/java/com/Ihsan/ujikom/activity/LoginActivity.kt
@@ -1,12 +1,21 @@
 package com.Ihsan.ujikom.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.Ihsan.ujikom.R
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val textVIew = findViewById<TextView>(R.id.link_toSignUp)
+        textVIew.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, SignInActivity::class.java))
+        }
     }
 }
