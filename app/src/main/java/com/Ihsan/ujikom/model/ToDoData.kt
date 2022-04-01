86274e34cfb1be59c46ad382dcca3269bd90a227
mughits06 <mughitszufar@gmail.com>
2022-04-01 20:45:24 +0700

    test

--- app/src/main/java/com/Ihsan/ujikom/model/ToDoData.kt
+++ app/src/main/java/com/Ihsan/ujikom/model/ToDoData.kt
@@ -1,4 +1,18 @@
 package com.Ihsan.ujikom.model
 
-class ToDoData {
-}
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.Ihsan.ujikom.data.Priority
+import kotlinx.android.parcel.Parcelize
+
+@Entity(tableName ="todo_table")
+@Parcelize
+class ToDoData (
+    @PrimaryKey(autoGenerate = true)
+    var id:Int,
+    var title:String,
+    var priority: Priority,
+    var description:String
+
+):Parcelable
