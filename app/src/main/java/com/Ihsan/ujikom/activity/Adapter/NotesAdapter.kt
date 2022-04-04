11b5cb79f3ea015409c96dd003fcfa62677cfac6
macbook <alanw5660@gmail.com>
2022-04-04 14:43:05 +0700

    test

--- app/src/main/java/com/Ihsan/ujikom/activity/Adapter/NotesAdapter.kt
+++ app/src/main/java/com/Ihsan/ujikom/activity/Adapter/NotesAdapter.kt
@@ -1,20 +1,64 @@
 package com.Ihsan.ujikom.activity.Adapter
 
 import android.content.Context
-//import android.provider.ContactsContract
-import android.provider.ContactsContract.CommonDataKinds.Note
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.Ihsan.ujikom.R
+import com.Ihsan.ujikom.model.Notes
 
 
 class NotesAdapter (
         val context: Context,
         val noteClickInterface: NoteClickInterface,
-        val noteClickDeleteInterface: NoteClickDeleteInterface) {
+        val noteClickDeleteInterface: NoteClickDeleteInterface
+        ) : RecyclerView.Adapter<NotesAdapter.ViewHolder>(){
+
+        private val allNotes = ArrayList<Notes>()
+
+
+                inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+                        val noteTV = itemView.findViewById<TextView>(R.id.idTVNoteTitle)
+                        val timeTV = itemView.findViewById<TextView>(R.id.idTVTimeStamp)
+                        val deleteIV = itemView.findViewById<ImageView>(R.id.idTVDelete)
+                }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+                val itemView = LayoutInflater.from(parent.context).inflate(R.layout.note_rv_item, parent, false);
+                return ViewHolder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+                holder.noteTV.setText(allNotes.get(position).noteTitle)
+                holder.timeTV.setText("Last Update : "+ allNotes.get(position).timeStamp)
+
+                holder.deleteIV.setOnClickListener {
+                        noteClickDeleteInterface.onDeleteIconClick(allNotes.get(position))
+                }
+
+                holder.itemView.setOnClickListener {
+                        noteClickInterface.onNoteClick(allNotes.get(position))
+                }
+        }
+
+        override fun getItemCount(): Int {
+                return allNotes.size
+        }
+
+        fun updateList(newList : List<Notes>){
+                allNotes.clear()
+                allNotes.addAll(newList)
+                notifyDataSetChanged()
+        }
 }
 
 interface NoteClickDeleteInterface{
-        fun onDeleteIconClick(note: Note)
+        fun onDeleteIconClick(note: Notes)
 }
 
 interface NoteClickInterface{
-        fun onDeleteIconClick(note: Note)
+        fun onNoteClick(get: Notes)
 }
--- app/src/main/java/com/Ihsan/ujikom/activity/MainActivity.kt
+++ app/src/main/java/com/Ihsan/ujikom/activity/MainActivity.kt
@@ -1,24 +1,60 @@
 package com.Ihsan.ujikom.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.Ihsan.ujikom.R
-import com.Ihsan.ujikom.databinding.ActivityMainBinding
+import com.Ihsan.ujikom.activity.Adapter.NoteClickDeleteInterface
+import com.Ihsan.ujikom.activity.Adapter.NoteClickInterface
+import com.Ihsan.ujikom.activity.Adapter.NotesAdapter
+import com.Ihsan.ujikom.model.Notes
+import com.Ihsan.ujikom.viewmodel.NotesViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), NoteClickDeleteInterface, NoteClickInterface {
     lateinit var notesRV :RecyclerView
     lateinit var addNote : FloatingActionButton
+    lateinit var viewModel: NotesViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_main)
         notesRV  = findViewById(R.id.idRVNotes)
         addNote  = findViewById(R.id.idFABAddNote)
+        notesRV.layoutManager = LinearLayoutManager(this)
 
+        val noteRVAdapter = NotesAdapter(this, this, this)
+        notesRV.adapter = noteRVAdapter
+        viewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NotesViewModel::class.java)
+        viewModel.allNotes.observe(this, { list->
+            list?.let {
+                noteRVAdapter.updateList(it)
+            }
+        })
+        addNote.setOnClickListener {
+            val intent = Intent(this@MainActivity,AddEditNoteActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        }
     }
 
+    override fun onDeleteIconClick(note: Notes) {
+        viewModel.deleteNote(note)
+        Toast.makeText(this, "${note.noteTitle} Deleted", Toast.LENGTH_LONG).show()
+    }
 
+    override fun onNoteClick(note: Notes) {
+        val intent = Intent(this@MainActivity, AddEditNoteActivity::class.java)
+        intent.putExtra("noteType", "Edit")
+        intent.putExtra("noteTitle", note.noteTitle)
+        intent.putExtra("noteDescription", note.noteDesc)
+        intent.putExtra("noteID", note.id)
+        startActivity(intent)
+        finish()
+    }
 }
