e5f991f8ea692f3a70bddb67b6bc85993fcc8d55
macbook <alanw5660@gmail.com>
2022-04-04 13:31:05 +0700

    test

--- app/src/main/java/com/Ihsan/ujikom/activity/Adapter/NotesAdapter.kt
+++ app/src/main/java/com/Ihsan/ujikom/activity/Adapter/NotesAdapter.kt
@@ -1,10 +1,20 @@
 package com.Ihsan.ujikom.activity.Adapter
 
 import android.content.Context
+//import android.provider.ContactsContract
+import android.provider.ContactsContract.CommonDataKinds.Note
+
 
 class NotesAdapter (
         val context: Context,
+        val noteClickInterface: NoteClickInterface,
+        val noteClickDeleteInterface: NoteClickDeleteInterface) {
+}
 
-        ) {
+interface NoteClickDeleteInterface{
+        fun onDeleteIconClick(note: Note)
+}
 
+interface NoteClickInterface{
+        fun onDeleteIconClick(note: Note)
 }
